448a97c7655da9b3ecddb34bf41ba7c1b47ad723
Raunak173 <raunakagarwal173@gmail.com>
2022-05-16 20:33:47 +0530

    added signin capability

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -5,6 +5,30 @@ import UserModal from "../models/user.js";
 
 const secret = "test";
 
+export const signin = async (req, res) => {
+  const { email, password } = req.body; //Get email and password from frontend
+
+  try {
+    const oldUser = await UserModal.findOne({ email }); //He must have registered email
+    if (!oldUser)
+      return res.status(404).json({ message: "User doesn't exist" });
+
+    const isPasswordCorrect = await bcrypt.compare(password, oldUser.password); //Check if passwords are same.
+
+    if (!isPasswordCorrect)
+      return res.status(400).json({ message: "Invalid credentials" });
+
+    const token = jwt.sign({ email: oldUser.email, id: oldUser._id }, secret, {
+      expiresIn: "1h",
+    }); //Create a jwt token for him
+
+    res.status(200).json({ result: oldUser, token }); //Send the old user and token created
+  } catch (error) {
+    res.status(500).json({ message: "Something went wrong" });
+    console.log(error);
+  }
+};
+
 export const signup = async (req, res) => {
   const { email, password, firstName, lastName } = req.body; //First we are getting all the info from frontend i.e registration form using req.body
   try {
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,9 @@
 import express from "express";
 const router = express.Router();
 
-import { signup } from "../controllers/user.js";
+import { signup, signin } from "../controllers/user.js";
 
-router.post("/signup", signup); //Post route  to create new user for app
+router.post("/signup", signup); //Post route to create new user for app
+router.post("/signin", signin); //Post route so that user can login in our app
 
 export default router;
