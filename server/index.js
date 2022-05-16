73eba4533737f4b14c012d0fc6e286fe71e7764f
Raunak173 <raunakagarwal173@gmail.com>
2022-05-16 20:02:14 +0530

    added models

--- server/index.js
+++ server/index.js
@@ -1,12 +1,23 @@
-import express from "express";
-const PORT = 5000;
+import express, { urlencoded } from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
+
+const port = 5000;
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello backend");
-});
+app.use(morgan("dev"));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+const MONGODB_URI =
+  "mongodb+srv://raunak173:raunak173@cluster0.achg4.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    app.listen(port, () => console.log(`Server running on port ${port}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
