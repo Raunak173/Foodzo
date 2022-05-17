b0b39723af89bc412b1449830e22d100a544e389
Raunak173 <raunakagarwal173@gmail.com>
2022-05-17 11:23:26 +0530

    registration function added

--- client/src/redux/api.js
+++ client/src/redux/api.js
@@ -3,3 +3,4 @@ import axios from "axios";
 const API = axios.create({ baseURL: "http://localhost:5000" }); //creates a api with following base url
 
 export const signIn = (formData) => API.post("/users/signin", formData);
+export const signUp = (formData) => API.post("/users/signup", formData);
--- client/src/redux/features/authSlice.js
+++ client/src/redux/features/authSlice.js
@@ -14,6 +14,21 @@ export const login = createAsyncThunk(
     }
   }
 );
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async ({ formValue, navigate, toast }, { rejectWithValue }) => {
+    try {
+      const response = await api.signUp(formValue);
+      toast.success("Registered Successfully!");
+      navigate("/");
+      return response.data;
+    } catch (err) {
+      return rejectWithValue(err.response.data);
+    }
+  }
+);
+
 const authslice = createSlice({
   name: "auth",
   initialState: {
@@ -31,6 +46,18 @@ const authslice = createSlice({
       state.user = action.payload;
     },
     [login.rejected]: (state, action) => {
+      state.loading = false;
+      state.error = action.payload.message;
+    },
+    [register.pending]: (state, action) => {
+      state.loading = true;
+    },
+    [register.fulfilled]: (state, action) => {
+      state.loading = false;
+      localStorage.setItem("profile", JSON.stringify({ ...action.payload }));
+      state.user = action.payload;
+    },
+    [register.rejected]: (state, action) => {
       state.loading = false;
       state.error = action.payload.message;
     },
