49ada9fd7d6aac4c4a6bde24f56aa2d42aff9263
Raunak173 <raunakagarwal173@gmail.com>
2022-05-17 20:13:08 +0530

    added spinner and card component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,7 +1,75 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { MDBCol, MDBContainer, MDBRow, MDBTypography } from "mdb-react-ui-kit";
+import { useDispatch, useSelector } from "react-redux";
+import { getFoods, setCurrentPage } from "../redux/features/foodSlice";
+import CardFood from "../components/CardFood";
+import Spinner from "../components/Spinner";
+import Pagination from "../components/Pagination";
+import { useLocation } from "react-router-dom";
 
-function Home() {
-  return <div>Home</div>;
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
 }
+
+const Home = () => {
+  const { foods, loading, currentPage, numberOfPages } = useSelector(
+    (state) => ({
+      ...state.food,
+    })
+  );
+  const dispatch = useDispatch();
+  const query = useQuery();
+  const searchQuery = query.get("searchQuery");
+  const location = useLocation();
+
+  useEffect(() => {
+    dispatch(getFoods(currentPage));
+  }, [currentPage]);
+
+  if (loading) {
+    return <Spinner />;
+  }
+  return (
+    <div
+      style={{
+        margin: "auto",
+        padding: "15px",
+        maxWidth: "1000px",
+        alignContent: "center",
+      }}
+    >
+      <MDBRow className="mt-5">
+        {foods.length === 0 && location.pathname === "/" && (
+          <MDBTypography className="text-center mb-0" tag="h2">
+            No Foods Found :/
+          </MDBTypography>
+        )}
+
+        {foods.length === 0 && location.pathname !== "/" && (
+          <MDBTypography className="text-center mb-0" tag="h2">
+            We couldn't find any matches for "{searchQuery}"
+          </MDBTypography>
+        )}
+
+        <MDBCol>
+          <MDBContainer>
+            <MDBRow className="row-cols-1 row-cols-md-3 g-2">
+              {foods &&
+                foods.map((item) => <CardFood key={item._id} {...item} />)}
+            </MDBRow>
+          </MDBContainer>
+        </MDBCol>
+      </MDBRow>
+      {foods.length > 0 && !searchQuery && (
+        <Pagination
+          setCurrentPage={setCurrentPage}
+          numberOfPages={numberOfPages}
+          currentPage={currentPage}
+          dispatch={dispatch}
+        />
+      )}
+    </div>
+  );
+};
 
 export default Home;
